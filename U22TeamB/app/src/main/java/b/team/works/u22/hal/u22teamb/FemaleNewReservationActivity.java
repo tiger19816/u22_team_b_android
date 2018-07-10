780f10a46836635c878d4ce389b251327dbdd235
高田穂乃花 <kikusui1kaminasizuki0@gmail.com>
2018-07-10 20:40:59 +0900

    #6 Activity作成
    Activityの作成しました。

---
+++ U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/FemaleNewReservationActivity.java
@@ -0,0 +1,13 @@
+package b.team.works.u22.hal.u22teamb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FemaleNewReservationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_female_new_reservation);
+    }
+}
