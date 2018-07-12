66c177a4c2968e3f3c628364f6d77d37c91db63c
高田穂乃花 <kikusui1kaminasizuki0@gmail.com>
2018-07-12 10:40:18 +0900

    #6 ツールバー編集。
    ツールバーの編集。

--- U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/FemaleNewReservationActivity.java
+++ U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/FemaleNewReservationActivity.java
@@ -1,9 +1,16 @@
 package b.team.works.u22.hal.u22teamb;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class FemaleNewReservationActivity extends AppCompatActivity {
+public class FemaleNewReservationActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +18,49 @@ public class FemaleNewReservationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_female_new_reservation);
 
         setTitle( "予約内容入力" );
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        DrawerLayout drawer = findViewById(R.id.dlMainContent);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nvSideMenuButton);
+        navigationView.setNavigationItemSelectedListener( this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.female_new_reservation_optionmenu, menu);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.dlMainContent);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_map) {
+        } else if (id == R.id.nav_reservation) {
+        } else if (id == R.id.nav_history) {
+        } else if (id == R.id.nav_my_page) {
+        }
+
+        DrawerLayout drawer = findViewById(R.id.nvSideMenuButton);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
