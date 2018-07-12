54f8c57753583b4597663156442f2070ec1ab217
高田穂乃花 <kikusui1kaminasizuki0@gmail.com>
2018-07-12 12:15:20 +0900

    #6 レフトナビを削除
    サンプルとして記述していたレフトナビを削除しました。
    それと「次へ」ボタンを下に合わせて配置。

--- U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/FemaleNewReservationActivity.java
+++ U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/FemaleNewReservationActivity.java
@@ -1,16 +1,9 @@
 package b.team.works.u22.hal.u22teamb;
 
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class FemaleNewReservationActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class FemaleNewReservationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,62 +12,11 @@ public class FemaleNewReservationActivity extends AppCompatActivity implements N
 
         setTitle( "予約内容入力" );
 
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-
-//        DrawerLayout drawer = findViewById(R.id.dlMainContent);
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        drawer.addDrawerListener(toggle);
-//        toggle.syncState();
-
-        //左のメインメニュー。
-        NavigationView navigationView = findViewById(R.id.nvSideMenuButton);
-        navigationView.setNavigationItemSelectedListener( this);
-    }
-
-    /**
-     * オプションメニュー作成。
-     * @param menu
-     * @return
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.female_new_reservation_optionmenu, menu);
-        return true;
-    }
-
-    /**
-     * 左サイドメニュー以外をクリックした時。
-     */
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.dlMainContent);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    /**
-     * 左サイドメニューのボタンをクリックした時。
-     * @param item
-     * @return
-     */
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.nav_map) {
-        } else if (id == R.id.nav_reservation) {
-        } else if (id == R.id.nav_history) {
-        } else if (id == R.id.nav_my_page) {
+        // アクションバーに前画面に戻る機能をつける
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        DrawerLayout drawer = findViewById(R.id.nvSideMenuButton);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
