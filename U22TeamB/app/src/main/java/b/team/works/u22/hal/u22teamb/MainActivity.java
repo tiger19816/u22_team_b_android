1b5d8b6123113c1770e838c1070b103d463a596a
高田穂乃花 <kikusui1kaminasizuki0@gmail.com>
2018-07-13 11:03:06 +0900

    #3 画面設計とjavaファイル編集
    画面設計はとりあえず、これで完成だと思います。javaは、IDとパスワードチェックの処理はまだ書けていません。

--- U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/MainActivity.java
+++ U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/MainActivity.java
@@ -1,13 +1,33 @@
 package b.team.works.u22.hal.u22teamb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String USER_TYPE = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onNewUserPageClick(View view){
+
+//        Intent intent;
+//
+//        if ("".equals("")) {
+//            //ログインが妻の時。
+//            USER_TYPE = "FEMALE";
+//            intent = new Intent(MainActivity.this , FemaleStoreMapListActivity.class);
+//        }else{
+//            //ログインが夫の時。
+//            USER_TYPE = "MALE";
+//            intent = new Intent(MainActivity.this , MaleReservationListActivity.class);
+//        }
+//        startActivity(intent);
     }
 }
