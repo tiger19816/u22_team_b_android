b65c7157795ffb204e4614512deb00181254f91c
高田穂乃花 <kikusui1kaminasizuki0@gmail.com>
2018-07-15 00:23:46 +0900

    #7 画面設計は完了
    画面設計は完了。あとは色次第。

--- U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/MainActivity.java
+++ U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/MainActivity.java
@@ -10,9 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(MainActivity.this , FemaleFinishReservationActivity.class);
-        startActivity(intent);
-
     }
 }
