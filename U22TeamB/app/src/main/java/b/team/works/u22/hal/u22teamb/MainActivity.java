76d0947dd05887bb557d1e33458219c442e75e35
takada <kikusui1kaminasizuki0@gmail.com>
2018-07-17 17:47:53 +0900

    #3 画面01【妻&夫】ログイン画面の画面設計ができました。 (#20)
    * ログイン画面の初期編集
    
    初めての、編集。
    
    * ログイン画面の編集サンプル
    
    ログイン画面とstyle.xmlを試しに編集。
    
    * #3 画面設計編集
    
    画面設計をしました。
    
    * #3 画面設計とjavaファイル編集
    
    画面設計はとりあえず、これで完成だと思います。javaは、IDとパスワードチェックの処理はまだ書けていません。
    
    * #3 ログイン機能を記述
    
    ログイン機能のソースを記述しました。
    あとはDBとの接続込みとなります。

--- U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/MainActivity.java
+++ U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/MainActivity.java
@@ -1,18 +1,49 @@
 package b.team.works.u22.hal.u22teamb;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String USER_TYPE = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        Intent intent = new Intent(MainActivity.this , FemaleNewReservationActivity.class);
-        startActivity(intent);
+    public void onUserLoginClick(View view){
 
+        //ユーザーID
+        EditText etId = findViewById(R.id.etId);
+        String strId = etId.getText().toString();
+        //パスワード
+        EditText etPassword = findViewById(R.id.etPassword);
+        String strPassword = etPassword.getText().toString();
+
+        String strMessage = "ここにエラーチェックの結果により、メッセージを記述。";
+        //DBチェック
+
+        //DBチェックの結果により、画面遷移先を変更。
+        Intent intent;
+        if ("FEMALE".equals(USER_TYPE)) {
+            //妻がログインした時。
+            USER_TYPE = "FEMALE";
+//            intent = new Intent(MainActivity.this , FemaleStoreMapListActivity.class);
+//            startActivity(intent);
+        }else if ("MALE".equals(USER_TYPE)) {
+            USER_TYPE = "MALE";
+            //夫がログインした時。
+//            intent = new Intent(MainActivity.this , MaleReservationListActivity.class);
+//            startActivity(intent);
+        }else{
+            Toast.makeText(MainActivity.this , strMessage , Toast.LENGTH_SHORT).show();
+        }
     }
 }
