0f9acc50612a339a25b6bb2f172d5e3731c325da
YoshiokaTomonori <36213889+YoshiokaTomonori@users.noreply.github.com>
2018-07-17 18:20:19 +0900

    YoshiokaComit

--- U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/FemaleNewMemberRegistrationActivity.java
+++ U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/FemaleNewMemberRegistrationActivity.java
@@ -3,12 +3,11 @@ package b.team.works.u22.hal.u22teamb;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FemaleNewMemberRegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.female_new_member_registration);
-
     }
 }
--- U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/FemaleNewMemberRegistrationConfirmationScreenActivity.java
+++ U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/FemaleNewMemberRegistrationConfirmationScreenActivity.java
@@ -3,12 +3,11 @@ package b.team.works.u22.hal.u22teamb;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FemaleNewMemberRegistrationConfirmationScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.female_new_member_registration);
-
+        setContentView(R.layout.female_new_member_registration_confirmation_screen);
     }
 }
