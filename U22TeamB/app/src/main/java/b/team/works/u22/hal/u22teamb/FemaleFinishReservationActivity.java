a74cc5c08b888c1fec882dc9be3480581b5989fd
takada <kikusui1kaminasizuki0@gmail.com>
2018-07-19 02:03:01 +0900

    #7【妻】予約の確認と決済画面の画面設計が完了 (#23)
    * #7 Activity作成
    
    Activityを作成しました。
    
    * #7 画面設計初期(画像のまま作成)
    
    * #7 スクロールビュー変更
    
    スクロールビューのコメントアウト。
    
    * #7 画面設計編集
    
    ボタンを下に寄せた。
    
    * #7 画面設計変更
    
    変更しました。
    一応できたと思います。
    
    * #7 画面設計は完了
    
    画面設計は完了。あとは色次第。

---
+++ U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/FemaleFinishReservationActivity.java
@@ -0,0 +1,15 @@
+package b.team.works.u22.hal.u22teamb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FemaleFinishReservationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_female_finish_reservation);
+
+        setTitle("予約内容確認");
+    }
+}
