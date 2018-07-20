344473e76a02b31fe0b5459e3a472fda0d691355
butio <34494262+butio@users.noreply.github.com>
2018-07-20 09:39:16 +0900

    #10 style変更

--- U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/MainActivity.java
+++ U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.male_additional_information_entry);
     }
 
     public void onUserLoginClick(View view){
