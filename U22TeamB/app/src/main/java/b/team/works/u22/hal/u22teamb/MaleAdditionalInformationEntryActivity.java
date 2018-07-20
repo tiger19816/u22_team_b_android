15385f003063db94d64bf8bb79b890bbd19c5054
takada <kikusui1kaminasizuki0@gmail.com>
2018-07-20 10:19:34 +0900

    #1 画面設計の完了 (#26)
    * テスト
    
    butio
    
    * YoshiokaComit

---
+++ U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/MaleAdditionalInformationEntryActivity.java
@@ -0,0 +1,13 @@
+package b.team.works.u22.hal.u22teamb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MaleAdditionalInformationEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.male_additional_information_entry);
+    }
+}
