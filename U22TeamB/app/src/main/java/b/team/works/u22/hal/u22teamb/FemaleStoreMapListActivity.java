9f2875cf7755741b33f5c7e8336278316e042897
tiger19816 <tiger19816@gmail.com>
2018-07-22 16:36:44 +0900

    Map追加完了

--- U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/FemaleStoreMapListActivity.java
+++ U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/FemaleStoreMapListActivity.java
@@ -1,6 +1,7 @@
 package b.team.works.u22.hal.u22teamb;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -10,13 +11,32 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
-public class FemaleStoreMapListActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class FemaleStoreMapListActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+
+    private ListView linearContentsArea;    // 内容エリア]
+    private LinearLayout linearLayoutArea;
+    private FloatingActionButton fab;
+    private final static int DURATION = 400;    // アニメーションにかける時間(ミリ秒)
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_female_store_map_list);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.storeMaps);
+        mapFragment.getMapAsync(this);
 
         setTitle("店一覧");
 
@@ -34,16 +54,42 @@ public class FemaleStoreMapListActivity extends AppCompatActivity implements Nav
         NavigationView navigationView = findViewById(R.id.nvSideMenuButton);
         navigationView.setNavigationItemSelectedListener(this);
 
+        // 内容エリアの結び付け
+        linearContentsArea = findViewById(R.id.lvStoreList);
+        // ボタンの結び付け
+        fab = findViewById(R.id.fabOpenList);
+
+        linearLayoutArea = findViewById(R.id.llStoreMapMain);
+
+        // ExpandするViewの元のサイズを保持
+        final int originalHeight = linearLayoutArea.getHeight() / 2;
+
+        // 内容エリアを閉じるアニメーション
+        FemaleStoreMapAnimation closeAnimation = new FemaleStoreMapAnimation(linearContentsArea, -originalHeight, originalHeight);
+        closeAnimation.setDuration(DURATION);
+        linearContentsArea.startAnimation(closeAnimation);
     }
 
+
     /**
-     *予約へのボタンがクリックされた時。
-     * @param view
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
      */
-    public void onNextReservationClick(View view){
-        Intent intent = new Intent(FemaleStoreMapListActivity.this,FemaleNewReservationActivity.class);
-        startActivity(intent);
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(34.699886, 135.493033);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("HAL大阪"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
 
     /**
      * レフトナビ以外をクリックした時の動き。
@@ -89,5 +135,47 @@ public class FemaleStoreMapListActivity extends AppCompatActivity implements Nav
         DrawerLayout drawer = findViewById(R.id.dlMainContent);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        // 内容エリアの結び付け
+        linearContentsArea = findViewById(R.id.lvStoreList);
+        // ボタンの結び付け
+        fab = findViewById(R.id.fabOpenList);
+
+        // ExpandするViewの元のサイズを保持
+        final int originalHeight = linearLayoutArea.getHeight() / 2;
+
+        // 展開ボタン押下時
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (linearContentsArea.getHeight() > 0) {       // 内容エリアが開いている時
+
+                    // 内容エリアを閉じるアニメーション
+                    FemaleStoreMapAnimation closeAnimation = new FemaleStoreMapAnimation(linearContentsArea, -originalHeight, originalHeight);
+                    closeAnimation.setDuration(DURATION);
+                    linearContentsArea.startAnimation(closeAnimation);
+                } else {
+
+                    // 内容エリアが閉じている時、内容エリアを開くアニメーション
+                    FemaleStoreMapAnimation openAnimation = new FemaleStoreMapAnimation(linearContentsArea, originalHeight, 0);
+                    openAnimation.setDuration(DURATION);    // アニメーションにかける時間(ミリ秒)
+                    linearContentsArea.startAnimation(openAnimation);   // アニメーション開始
+                }
+            }
+        });
+    }
+
+    /**
+     * フローティングアクションボタンが押された時のイベント処理用メソッド.
+     *
+     * @param view 画面部品。
+     */
+    public void onFabOpenListClick(View view) {
     }
 }
