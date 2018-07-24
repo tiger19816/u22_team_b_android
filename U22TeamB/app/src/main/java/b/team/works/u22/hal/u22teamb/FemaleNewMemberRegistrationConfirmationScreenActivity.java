0033c82d22ea438a71c16609c8bb06e2b215bb28
高田穂乃花 <kikusui1kaminasizuki0@gmail.com>
2018-07-24 15:14:18 +0900

    #29 コメントアウト文変更

--- U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/FemaleNewMemberRegistrationConfirmationScreenActivity.java
+++ U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/FemaleNewMemberRegistrationConfirmationScreenActivity.java
@@ -30,7 +30,7 @@ import java.net.URL;
 public class FemaleNewMemberRegistrationConfirmationScreenActivity extends AppCompatActivity {
 
     /**
-     * ログインする先のURL。
+     * URL。
      */
     private static final String LOGIN_URL = "http://10.0.2.2:8080/U22Verification/Servlet";
 
