0eccefd9503e947ea9fb9b79305acf5a1e6f86b9
高田穂乃花 <kikusui1kaminasizuki0@gmail.com>
2018-07-25 01:42:41 +0900

    #29 MyPageにレフトナビ追加

--- U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/FemaleMyPageActivity.java
+++ U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/FemaleMyPageActivity.java
@@ -1,13 +1,83 @@
 package b.team.works.u22.hal.u22teamb;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class FemaleMyPageActivity extends AppCompatActivity {
+public class FemaleMyPageActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_female_my_page);
+
+        setTitle("MyPage");
+
+        //ツールバー(レイアウトを変更可)。
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //DrawerLayout
+        DrawerLayout drawer = findViewById(R.id.dlMainContent);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //レフトナビ本体。
+        NavigationView navigationView = findViewById(R.id.nvSideMenuButton);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
+    /**
+     * レフトナビ以外をクリックした時の動き。
+     */
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.dlMainContent);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    /**
+     * レフトナビをクリックした時。
+     * @param item
+     * @return
+     */
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        Intent intent;
+        if (id == R.id.nav_map) {
+            intent = new Intent(FemaleMyPageActivity.this,FemaleStoreMapListActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_reservation) {
+            intent = new Intent(FemaleMyPageActivity.this,FemaleReservationListActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_history) {
+            intent = new Intent(FemaleMyPageActivity.this,FemaleHistoryListActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_my_page) {
+            intent = new Intent(FemaleMyPageActivity.this,FemaleMyPageActivity.class);
+            startActivity(intent);
+        }else if (id == R.id.nav_logout){
+            intent = new Intent(FemaleMyPageActivity.this,MainActivity.class);
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = findViewById(R.id.dlMainContent);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
