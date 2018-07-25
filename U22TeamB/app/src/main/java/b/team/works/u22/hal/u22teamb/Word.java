2ddf9c228e1424709f4ee6c93619be20fe03cf10
tiger19816 <tiger19816@gmail.com>
2018-07-26 07:05:11 +0900

    マップレイアウト仮完成

--- U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/Word.java
+++ U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/Word.java
@@ -6,7 +6,7 @@ package b.team.works.u22.hal.u22teamb;
  * @author Taiga Hirai
  */
 public class Word {
-    private static final String SERVER_URL = "http://192.168.1.106:8080/U22TeamB/";
+    private static final String SERVER_URL = "http://192.168.1.20:8080/U22TeamB/";
     public static final String STORE_MAP_URL = SERVER_URL + "StoreListServlet";
     public static final String STORE_DETAILS_URL = SERVER_URL + "StoreDetailsServlet";
 }
