7aff0bb6eee4b4d6d3449e3c4d5e8d5781a245ef
高田穂乃花 <kikusui1kaminasizuki0@gmail.com>
2018-08-07 00:01:15 +0900

    #29 変更

---
+++ U22TeamBStore/app/src/main/java/b/team/works/u22/hal/u22teambstore/Word.java
@@ -0,0 +1,15 @@
+package b.team.works.u22.hal.u22teambstore;
+
+/**
+ * 定数を集めたクラス。
+ *
+ * @author Honoka Takada
+ */
+
+public class Word {
+
+    //    private static final String SERVER_URL = "http://192.168.1.20:8080/U22TeamB/";
+    private static final String SERVER_URL = "http://10.0.2.2:8080/u22_team_b_web/";
+    public static final String USER_LOGIN_URL = SERVER_URL + "StoreLoginJsonServlet";
+
+}
