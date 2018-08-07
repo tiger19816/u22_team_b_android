9f67534c92745f1711184ac32b2391f45e0d7a1f
高田穂乃花 <kikusui1kaminasizuki0@gmail.com>
2018-08-07 15:08:24 +0900

    #46 夫追加情報追加画面を変更

--- U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/MaleAdditionalInformationEntryActivity.java
+++ U22TeamB/app/src/main/java/b/team/works/u22/hal/u22teamb/MaleAdditionalInformationEntryActivity.java
@@ -2,6 +2,8 @@ package b.team.works.u22.hal.u22teamb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MaleAdditionalInformationEntryActivity extends AppCompatActivity {
 
@@ -9,5 +11,32 @@ public class MaleAdditionalInformationEntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.male_additional_information_entry);
+
+        setTitle("追加情報");
+
+        //ツールバー(レイアウトを変更可)。
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // アクションバーに前画面に戻る機能をつける
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    /**
+     * アクションバー。
+     * @param item
+     * @return boolean
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
