58b518ecd997967e83cb0295d8aea75f74b2d766
Punme <Punme@users.noreply.github.com>
2018-08-24 01:41:24 +0900

    #35 プルリクエスト 2018/8/23 ① (#58)
    * #35 予約リスト。 ブランチ作成。
    予約リストの表示アクティビティは「ReservationListActivity.class」。
    
    ToDo: リストビューを作成し、予約リストを表示。リストビューをタップすると、QRコードリーダを起動する機能の実装。
    
    * ・店IDに対応した予約一覧。
    ・夫IDから予約詳細を取得。
    ・操作によってサーブレットへ予約IDを送信し、一覧へ戻る。
    
    * プルリクエスト用のコミット。
    
    ・QRコード読取から来店時の処理まで
    ・店IDに対応した予約一覧。
    
    TODO:妻に通知。

--- U22TeamBStore/app/src/main/java/b/team/works/u22/hal/u22teambstore/Word.java
+++ U22TeamBStore/app/src/main/java/b/team/works/u22/hal/u22teambstore/Word.java
@@ -12,4 +12,9 @@ public class Word {
     private static final String SERVER_URL = "http://10.0.2.2:8080/u22_team_b_web/";
     public static final String USER_LOGIN_URL = SERVER_URL + "StoreLoginJsonServlet";
 
+    //ReservationListActivityとReservationDetailActivityのURL。 by Yuki Yoshida TODO:適切なURLに変更。
+    private static final String MY_URL = "http://10.0.2.2:8080/team_b_web/";
+    public static final String RECEIVE_RESERVATION_LIST = MY_URL + "SendReservationListServlet";
+    public static final String RECEIVE_RESERVATION_DETAIL = MY_URL + "SendReservationDetailServlet";
+    public static final String PROCESS_OF_VISIT = MY_URL + "UpdateVisitFlagServlet";
 }
