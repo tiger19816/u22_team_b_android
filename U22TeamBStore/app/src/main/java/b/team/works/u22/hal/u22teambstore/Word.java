a2ba0994592920e078bba0cd94ba763d4dbdeaa3
高田穂乃花 <kikusui1kaminasizuki0@gmail.com>
2018-08-28 16:56:43 +0900

    #46 訂正

--- U22TeamBStore/app/src/main/java/b/team/works/u22/hal/u22teambstore/Word.java
+++ U22TeamBStore/app/src/main/java/b/team/works/u22/hal/u22teambstore/Word.java
@@ -8,13 +8,8 @@ package b.team.works.u22.hal.u22teambstore;
 
 public class Word {
 
-<<<<<<< HEAD
-//    private static final String SERVER_URL = "http://10.0.2.2:8080/u22_team_b_web/";//localhostアドレス
     private static final String SERVER_URL = "http://59.106.219.240:8080/u22_team_b_web/";
-=======
-    private static final String SERVER_URL = "http://10.0.2.2:8080/u22_team_b_web/";//localhostアドレス
-//    private static final String SERVER_URL = "http://59.106.219.240:8080/u22_team_b_web/";
->>>>>>> master
+//    private static final String SERVER_URL = "http://10.0.2.2:8080/u22_team_b_web/";//localhostアドレス
     public static final String USER_LOGIN_URL = SERVER_URL + "StoreLoginJsonServlet";
     public static final String RECEIVE_RESERVATION_LIST = SERVER_URL + "SendReservationListServlet";
     public static final String RECEIVE_RESERVATION_DETAIL = SERVER_URL + "SendReservationDetailServlet";
