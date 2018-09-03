dc834f085c3b04b5223a630907c970e4f30f4dc3
takada <kikusui1kaminasizuki0@gmail.com>
2018-09-03 09:18:24 +0900

    #46一旦コミット (#92)
    * #46 ログアウト機能追加
    
    * #46 夫追加情報追加画面を変更
    
    * #46 夫側新規登録追加
    
    * #46 QRコードの機能変更
    
    * #46 追加
    
    * #46 QRコード表示関連終了
    
    * #46 夫新規登録完了
    
    * #46 マージ訂正
    
    * #46 テンプレートの色を追加
    
    ここからの色作業は基本、コピペ。
    
    * #46 ユーザー新規登録調整
    
    * #46 デザイン完了
    
    * #46 デザイン調整
    
    * #46 訂正
    
    * #46 GIF画像表示用
    
    * #46 アニメーション用下地処理
    
    * #46 コミット
    
    * #46 マージ時の訂正
    
    * #46 GIF画像を表示する処理完成。
    
    GIF画像を表示する際の処理が完成。
    後は、コメントを表示する処理を追加するだけ。
    
    * #46 予約情報にメッセージ追加
    
    * #46 アプリのアイコン画像追加
    
    * #46 調整とGIF画像表示時の調整
    
    * #46 マージんの調整
    
    * #46 ログアウト機能追加
    
    * #46 ログイン修正
    
    * #46 コミット
    
    * #46 訂正
    
    * #46 調整
    
    * #46 訂正
    
    * #46 訂正
    
    * #46 訂正
    
    * #46 訂正
    
    ・ユーザーの新規登録(妻側)
    ・マイページ
    ・予約リスト(妻側)
    
    * #46 マイページオプションメニュー訂正
    
    * #46 変更
    
    * #46 調整
    
    * #46 調整
    
    * #46 エラー訂正
    
    * #46 新規予約情報項目追加
    
    * #46 価格調整
    
    * #46 調整
    
    * #46 様々な調整
    
    * #46 店のリストの調整。
    
    * #46 調整
    
    * #46 ユーザー新規登録エラー修正
    
    * #46 夫新規登録調整
    
    * #46 GIF画像追加
    
    * #46 調整
    
    * #46 GIF画像追加
    
    * #46 調整
    
    * #46 店の新規登録処理完了
    
    * Loading追加&緯度経度追加
    
    * #46 調整
    
    * #46 一旦コミット
    
    * #46 コミット

--- U22TeamBStore/app/src/main/java/b/team/works/u22/hal/u22teambstore/DataConversion.java
+++ U22TeamBStore/app/src/main/java/b/team/works/u22/hal/u22teambstore/DataConversion.java
@@ -3,8 +3,13 @@ package b.team.works.u22.hal.u22teambstore;
 import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
+import android.util.Log;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -13,6 +18,19 @@ import java.util.Locale;
  */
 
 public class DataConversion {
+
+    private static final SimpleDateFormat dfFullDate01 = new SimpleDateFormat("yyyy年MM月dd日 hh時mm分");
+    private static final SimpleDateFormat dfFullDate02 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    private static final SimpleDateFormat dfDate01 = new SimpleDateFormat("yyyy年MM月dd日");
+    private static final SimpleDateFormat dfDate02 = new SimpleDateFormat("yyyy-MM-dd");
+    private static final SimpleDateFormat dfDate03 = new SimpleDateFormat("yyyy");
+    private static final SimpleDateFormat dfDate04 = new SimpleDateFormat("MM");
+    private static final SimpleDateFormat dfDate05 = new SimpleDateFormat("dd");
+    private static final SimpleDateFormat dfTime01 = new SimpleDateFormat("hh時mm分");
+    private static final SimpleDateFormat dfTime02 = new SimpleDateFormat("hh:mm:ss");
+    private static final SimpleDateFormat dfTime03 = new SimpleDateFormat("hh");
+    private static final SimpleDateFormat dfTime04 = new SimpleDateFormat("mm");
+    private static final SimpleDateFormat dfTime05 = new SimpleDateFormat("mm");
 
     /**
      * 住所から、緯度経度を取得するメソッド。
@@ -37,4 +55,5 @@ public class DataConversion {
 
         return latLong;
     }
+
 }
